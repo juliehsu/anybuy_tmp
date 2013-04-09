2d9ce9d7323906862c24bf699685e6a736638548
aizr <aizr1212@gmail.com>
2013-04-09 18:13:26 +0800

    renew message

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -21,4 +21,25 @@ class AboutController < ApplicationController
       end
     end
   end
+  
+  def edit 
+   @message = Message.find(params[:id])
+   
+   respond_to do |format|
+      if @message.update_attributes(params[:message])
+        format.html { redirect_to @message }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+  
+  def destroy
+  @message = Message.find(params[:id])
+    @message.destroy
+
+    respond_to do |format|
+      format.html { redirect_to :action => :message}
+    end
+  end
 end
