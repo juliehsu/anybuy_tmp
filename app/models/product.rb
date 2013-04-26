af6b7b8d9329c448b2298360683da4da6687afc5
aizr <aizr1212@gmail.com>
2013-04-26 17:58:39 +0800

    renew

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Product < ActiveRecord::Base
-   has_many :categoriess
+   has_many :categoriess, :dependent => :destroy
    has_many :images, :dependent => :destroy
    has_many :bids
    has_many :users, :through => :bids
 
    accepts_nested_attributes_for :images
    attr_accessible :title, :description, :price, :images_attributes, :winner_id, :end_at, :categories_id, :primary_price
-	 validates_presence_of :title, :description, :price, :primary_price
+	 validates_presence_of :title, :description, :price, :primary_price, :categories_id, :end_at
 	 validates_numericality_of :price, :primary_price, :only_integer => true
 end
